77fad2702308554dbd1c58425c74a94308fa0068
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-04-03 14:18:33 -0400

    set basic boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Basic Boilerplate</h1>
+      </main>
+    </>
+  );
+}
