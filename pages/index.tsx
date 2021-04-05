5746013361a1f0f3f6a0f2f0062f574eeea99493
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-04-05 16:41:32 -0400

    update product card style

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,6 @@ export default function Home({ products }: Props) {
             cart.map((product) => (
               <Stack
                 key={product.id}
-                spacing={3}
                 borderRadius="3xl"
                 backgroundColor="whiteAlpha.900"
               >
@@ -111,31 +110,50 @@ export default function Home({ products }: Props) {
                 />
 
                 <Stack padding={5}>
-                  <Heading size="md">{product.title}</Heading>
-                  <Text>{product.description}</Text>
+                  <Box minH={100}>
+                    <Heading size="lg">{product.title}</Heading>
+                    <Text color="GrayText">{product.description}</Text>
+                  </Box>
+                  <Box display="flex" justifyContent="space-between">
+                    <Text fontSize="md" fontWeight="bold">
+                      Precio
+                    </Text>
+                    <Text fontSize="md" fontWeight="bold">
+                      Cantidad
+                    </Text>
+                  </Box>
                   <Box
                     display="flex"
                     justifyContent="space-between"
                     alignItems="center"
                   >
-                    <Heading size="md" as="span" flexBasis={1}>
+                    <Heading size="lg" as="span" flexBasis={1}>
                       {parseCurrency(product.price)}
                     </Heading>
-                    <Button
-                      colorScheme="red"
-                      borderRadius="full"
-                      onClick={() => handleDecreaseProductQuantity(product.id)}
-                    >
-                      -
-                    </Button>
-                    <Text>{product.quantity}</Text>
-                    <Button
-                      colorScheme="red"
-                      borderRadius="full"
-                      onClick={() => handleIncreaseProductQuantity(product.id)}
-                    >
-                      +
-                    </Button>
+
+                    <Box display="flex" alignItems="center">
+                      <Button
+                        colorScheme="blackAlpha"
+                        borderRadius="full"
+                        onClick={() =>
+                          handleDecreaseProductQuantity(product.id)
+                        }
+                      >
+                        -
+                      </Button>
+                      <Text fontWeight="bold" paddingX={3}>
+                        {product.quantity}
+                      </Text>
+                      <Button
+                        colorScheme="red"
+                        borderRadius="full"
+                        onClick={() =>
+                          handleIncreaseProductQuantity(product.id)
+                        }
+                      >
+                        +
+                      </Button>
+                    </Box>
                   </Box>
                 </Stack>
               </Stack>
