c1d6a63d343cf5b591af67c373b6604bb80d33e4
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-04-22 21:02:12 -0400

    create _document.tx and add google adsense script tag

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            data-ad-client="ca-pub-3919957705186757"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
